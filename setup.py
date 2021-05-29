68c8fae5dae30dfc7da8d26b572af51df480b6c6
Aareon Sullivan <askully13@gmail.com>
2021-05-29 01:02:23 -0500

    Update setup.py
    Use `readme` and `requires` contexts

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from pathlib import Path
 
 from setuptools import setup
 
-requires = Path("requirements.txt").read_text().splitlines()
+requires = Path("requirements.txt")
 
-readme = Path("README.md").read_text()
+readme = Path("README.md")
 
 with Path("aiotnb/__init__.py").open("r") as f:
     version = re.search(r"^__version__ = \"([^\"]+)\"", f.read(), re.M).group(1)
 
 requires_optional = {
     "docs": [
-        "sphinx",
+        "sphinx",  # documentation
     ]
 }
 
@@ -27,10 +27,10 @@ setup(
     packages=[],
     license="MIT",
     description="A coroutine-based client for the TNB Network API",
-    long_description=readme,
+    long_description=readme.read_text().splitlines(),
     long_description_content_type="text/markdown",
     include_package_data=True,
-    install_requires=requires,
+    install_requires=requires.read_text().splitlines(),
     extras_require=requires_optional,
     python_requires=">=3.8.0",
     classifiers=[],
